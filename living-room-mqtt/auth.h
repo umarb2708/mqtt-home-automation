4c1e60a988db42bccb410f54ee8d797af3566d5d
Umar Windows <umarbasheer1996@gmail.com>
2021-06-03 22:15:28 +0530

    Home Automation

---
+++ living-room-mqtt/auth.h
@@ -0,0 +1,5 @@
+const char* ssid = "Umar";
+const char* password = "umarahammed";
+const char* serverHostname = "192.168.15.138";
+const String username = "HomeControl";
+const String pass = "Esp@REX123#";
---
+++ living-room-mqtt/living-room-mqtt.ino
@@ -0,0 +1,203 @@
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#elif defined(ESP32)
+#include <WiFi.h>
+#else
+#error "Board not found"
+#endif
+
+#include <PubSubClient.h>
+
+//Relays for switching appliances
+#define Relay1            15
+#define Relay2            2
+#define Relay3            4
+#define Relay4            22
+
+// Update these with values suitable for your network.
+const char* ssid = "Umar";
+const char* password = "umarahammed";
+const char* mqtt_server = "192.168.15.138"; // Local IP address of Raspberry Pi
+
+const char* username = "HomeControll";
+const char* pass = "Esp@REX123#";
+// Subscribed Topics
+
+#define sub1 "first/living/light"
+#define sub2 "first/living/fan"
+#define sub3 "first/living/lamp"
+//#define sub4 "device1/relay4"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+
+// Connecting to WiFi Router
+
+void setup_wifi()
+{
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+
+  if (strstr(topic, sub1))
+  {
+    for (int i = 0; i < length; i++)
+    {
+      Serial.print((char)payload[i]);
+    }
+    Serial.println();
+    // Switch on the LED if an 1 was received as first character
+    if ((char)payload[0] == '1')
+    {
+      digitalWrite(Relay1, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+      // but actually the LED is on; this is because
+      // it is active low on the ESP-01)
+    } else {
+      digitalWrite(Relay1, LOW);  // Turn the LED off by making the voltage HIGH
+    }
+  }
+
+  else if ( strstr(topic, sub2))
+  {
+    for (int i = 0; i < length; i++) {
+      Serial.print((char)payload[i]);
+    }
+    Serial.println();
+    // Switch on the LED if an 1 was received as first character
+    if ((char)payload[0] == '1') {
+      digitalWrite(Relay2, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+      // but actually the LED is on; this is because
+      // it is active low on the ESP-01)
+    } else {
+      digitalWrite(Relay2, LOW);  // Turn the LED off by making the voltage HIGH
+    }
+  }
+  else if ( strstr(topic, sub3))
+  {
+    for (int i = 0; i < length; i++) {
+      Serial.print((char)payload[i]);
+    }
+    Serial.println();
+    // Switch on the LED if an 1 was received as first character
+    if ((char)payload[0] == '1') {
+      digitalWrite(Relay3, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+      // but actually the LED is on; this is because
+      // it is active low on the ESP-01)
+    } else {
+      digitalWrite(Relay3, LOW);  // Turn the LED off by making the voltage HIGH
+    }
+  }
+/*  else if ( strstr(topic, sub4))
+  {
+    for (int i = 0; i < length; i++) {
+      Serial.print((char)payload[i]);
+    }
+    Serial.println();
+    // Switch on the LED if an 1 was received as first character
+    if ((char)payload[0] == '1') {
+      digitalWrite(Relay4, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+      // but actually the LED is on; this is because
+      // it is active low on the ESP-01)
+    } else {
+      digitalWrite(Relay4, LOW);  // Turn the LED off by making the voltage HIGH
+    }
+  }*/
+
+  else
+  {
+    Serial.println("unsubscribed topic");
+  }
+
+}
+
+
+// Connecting to MQTT broker
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str() , username, pass)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe(sub1);
+      client.subscribe(sub2);
+      client.subscribe(sub3);
+     //client.subscribe(sub4);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+
+void setup()
+{
+
+  pinMode(Relay1, OUTPUT);
+  pinMode(Relay2, OUTPUT);
+  pinMode(Relay3, OUTPUT);
+ // pinMode(Relay4, OUTPUT);
+
+
+  Serial.begin(115200);
+
+  setup_wifi();
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop()
+{
+
+  if (!client.connected()) 
+  {
+    reconnect();
+  }
+  client.loop();
+
+}
